9755bae32199bab4180b1c97f66e256306717f6a
Akash Singh <akashsingh6280@gmail.com>
2021-09-06 08:28:49 +0530

    ADDED: function to create and intialize array

--- src/com/bridgelabz/tictactoeproblem/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoeproblem/TicTacToeGame.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.tictactoeproblem;
 
 public class TicTacToeGame {
+	static char[] signArray=new char[10];
+	
+	public static void createArray() {
+		for(int index=1;index<10;index++) {
+			signArray[index]=' ';
+		}
+	}
 	public static void main(String[] args) {
-		System.out.println("welcome to game");
+		createArray();
+		
 	}
 }
