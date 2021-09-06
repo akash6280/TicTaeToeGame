75bbaaf010f0afdd8e41b03e25dc050e24ce659b
Akash Singh <akashsingh6280@gmail.com>
2021-09-06 08:50:54 +0530

    ADDED: Taking input choice from user

--- src/com/bridgelabz/tictactoeproblem/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoeproblem/TicTacToeGame.java
@@ -1,15 +1,36 @@
 package com.bridgelabz.tictactoeproblem;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	static char[] signArray=new char[10];
+	static Scanner scanner=new Scanner(System.in);
+	static char[] board=new char[10];
+	static char playerLetter;
+	static char computerLetter;
 	
-	public static void createArray() {
+	public static void createBoard() {
 		for(int index=1;index<10;index++) {
-			signArray[index]=' ';
+			board[index]=' ';
 		}
 	}
+	public static void inputChoice() {
+		System.out.println("Enter user choice");
+		char choice=scanner.next().charAt(0);
+		if(choice=='X') {
+			playerLetter=choice;
+			computerLetter='O';
+		}
+		else{
+			playerLetter=choice;
+			computerLetter='X';
+			
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		createArray();
+		createBoard();
+		inputChoice();
 		
 	}
 }
