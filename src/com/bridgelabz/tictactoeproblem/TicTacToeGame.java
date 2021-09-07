134d9b51d2b95d6dd9b47ab7f7f03b5b91f9a186
Akash Singh <akashsingh6280@gmail.com>
2021-09-07 21:16:25 +0530

    ADDED: method to check if user win or tie happens

--- src/com/bridgelabz/tictactoeproblem/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoeproblem/TicTacToeGame.java
@@ -7,6 +7,9 @@ public class TicTacToeGame {
 	static char[] board=new char[10];
 	static char playerLetter;
 	static char computerLetter;
+	static char currentPlayer;
+	static int positionRemainnig=9;
+	static int [][] winningPositions = {{1,2,3}, {4,5,6}, {7,8,9}, {1,4,7}, {2,5,8}, {3,6,9},{1,5,9}, {3,5,7}};
 	
 	public static void createBoard() {
 		for(int index=1;index<10;index++) {
@@ -63,11 +66,29 @@ public class TicTacToeGame {
 		
 		if(tossResult==tossChoice) {
 			System.out.println("user play first");
+			currentPlayer='X';
 		}
 		else {
 			System.out.println("Computer plays first");
+			currentPlayer='O';
 		}
 	}
+	public static void checkWinner(){
+		if(positionRemainnig == 0) {
+			System.out.println("the game ended in a tie");
+			return;
+		}
+		
+		for(int i=0;i<8;i++) {
+			if(board[winningPositions[i][0]] == currentPlayer && board[winningPositions[i][1]] == currentPlayer && board[winningPositions[i][2]] == currentPlayer) {
+				System.out.println("The "+(currentPlayer == 1?"User":"Computer")+" is the winner****");
+				return;
+			}
+		}
+		currentPlayer = (currentPlayer=='X')?'0':'X';
+	}
+	
+	
 
 	public static void main(String[] args) {
 		createBoard();
@@ -75,6 +96,7 @@ public class TicTacToeGame {
 		showBoard();
 		playerMove();
 		decideFirstPlayer();
+		checkWinner();
 		
 	}
 }
